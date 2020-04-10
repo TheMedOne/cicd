201a08fa6f4ed14891a834b32bdce4ed81a71853
EL MEHDI AARAB <42239528+TheMedOne@users.noreply.github.com>
2020-04-10 03:13:36 +0100

    Update hello.go

--- hello.go
+++ hello.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello world medone")
+		c.String(http.StatusOK, "hello world medone yeeeep")
 	})
 
 	r.Run(":8888")
