8ac04716e33f326f5a001037f65325883ad05150
elmehdi-a <elmehdi.a@avito.ma>
2020-04-10 04:41:20 +0100

    ok

--- hello.go
+++ hello.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "   paris france yep ")
+		c.String(http.StatusOK, " hello  paris france yep ")
 	})
 
 	r.Run(":8888")
