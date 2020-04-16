42e3097f06fa0ca1d156560141dc9db64e7391b9
elmehdi-a <elmehdi.a@avito.ma>
2020-04-16 16:38:10 +0200

    hot fix

--- hello.go
+++ hello.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "  wayeeeeeh it is done alkhawa deyali fehmtini ")
+		c.String(http.StatusOK, "  wayeeeeeh it is done alkhawa deyali fehmtini  alkhawa")
 	})
 
 	r.Run(":8888")
