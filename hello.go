369062bc865d80a7a4cb413ce7c5f61b44a81d81
EL MEHDI AARAB <42239528+TheMedOne@users.noreply.github.com>
2020-04-17 20:47:53 +0200

    update body

--- hello.go
+++ hello.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "  wayeeeeeh it is done alkhawa deyali fehmtini ")
+		c.String(http.StatusOK, "  Hello world k8s cluster --> Golang app server ")
 	})
 
 	r.Run(":8888")
