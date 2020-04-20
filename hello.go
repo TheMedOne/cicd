e9bc95924943f7e49050e7bceda4eb266f6731d5
EL MEHDI AARAB <42239528+TheMedOne@users.noreply.github.com>
2020-04-20 14:52:32 +0200

    update

--- hello.go
+++ hello.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "  Hello world k8s cluster --> Golang app server  production stage 1.0 without rollback")
+		c.String(http.StatusOK, "  Hello world k8s cluster --> Golang app server  production stage 1.0 update")
 	})
 
 	r.Run(":8888")
