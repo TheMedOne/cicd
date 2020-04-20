957a2456693d3130689e2e8f1e72efaff77b2c9c
elmehdi-a <elmehdi.a@avito.ma>
2020-04-20 14:15:03 +0200

    prod stage

--- hello.go
+++ hello.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "  Hello world k8s cluster --> Golang app server  production")
+		c.String(http.StatusOK, "  Hello world k8s cluster --> Golang app server  production stage")
 	})
 
 	r.Run(":8888")
