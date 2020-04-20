c3ec4b764b441f91227ee3a3ac60db31c841236f
elmehdi-a <elmehdi.a@avito.ma>
2020-04-20 16:54:22 +0200

    2345

--- hello.go
+++ hello.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, " Hello Hello world k8s cluster --> Golang app server MG  Paris v1.0")
+		c.String(http.StatusOK, " Hello Hello world 2345 k8s cluster --> Golang app server MG  Paris v1.0")
 	})
 
 	r.Run(":8888")
